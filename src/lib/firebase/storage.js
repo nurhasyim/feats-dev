4666fd6dcb219bd9096112e298399dac984f4bee
Nur Hasyim <nur.hasyim@gmail.com>
2024-05-31 05:39:51 +0700

    Implements upload image

--- src/lib/firebase/storage.js
+++ src/lib/firebase/storage.js
@@ -5,7 +5,21 @@ import { storage } from "@/src/lib/firebase/clientApp";
 import { updateRestaurantImageReference } from "@/src/lib/firebase/firestore";
 
 // Replace the two functions below
-export async function updateRestaurantImage(restaurantId, image) {}
+export async function updateRestaurantImage(restaurantId, image) {
+    if (!restaurantId) 
+        throw new Error("No restaurant ID was provided");
+    if (!image)
+        throw new Error("No image was provided");
 
-async function uploadImage(restaurantId, image) {}
+    const publicImageUrl = await uploadImage(restaurantId, image);
+    await updateRestaurantImageReference(restaurantId, publicImageUrl);
+    return publicImageUrl;
+}
+
+async function uploadImage(restaurantId, image) {
+    const filepath = `images/${restaurantId}/${image.name}`;
+    const imageRef = ref(storage, filepath);
+    await uploadBytesResumable(imageRef, image);
+    return await getDownloadURL(imageRef);
+}
 // Replace the two functions above
